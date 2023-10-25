a713ac553269ea886f82e00b2fab0ba6235bd29e
PaulinaWywrot <wywrot.paula@gmail.com>
2023-10-25 16:47:54 +0100

    add bootstrap classes for medium screen

--- client/src/Components/Heading.js
+++ client/src/Components/Heading.js
@@ -3,18 +3,16 @@ import girlImage from "../img/girl.png";
 
 const Heading = () => {
   return (
-    <div className="custom-header d-flex align-items-center justify-content-around">
+    <div className="custom-header d-flex flex-column flex-md-row align-items-center justify-content-md-around">
       <div>
-        <div>
-          <h2 className="h2-custom">Pin The Mood 😊 😔 🤩</h2>
-          <h3 className="pt-3 h3-custom">
-            Your Personal Recommendation Diary
-            <br /> with a Mood Twist
-          </h3>
-        </div>
+        <h2 className="h2-custom">Pin The Mood 😊 😔 🤩</h2>
+        <h3 className="pt-3 h3-custom">
+          Your Personal Recommendation Diary
+          <br /> with a Mood Twist
+        </h3>
       </div>
-      <div className="align-self-end">
-        <img src={girlImage} alt="girl" className="galinha" />
+      <div className="align-self-center mt-3 mt-md-0 text-center">
+        <img src={girlImage} alt="girl" className="galinha img-fluid" />
       </div>
     </div>
   );

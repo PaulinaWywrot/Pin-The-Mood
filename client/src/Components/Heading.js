84f01cd28462061e12ac3b6881b4b4d0915ba171
PaulinaWywrot <wywrot.paula@gmail.com>
2023-10-25 13:56:17 +0100

    update table and css
    -create  table with template data
    -replace header icon
    - update background colour

--- client/src/Components/Heading.js
+++ client/src/Components/Heading.js
@@ -1,4 +1,23 @@
+import React from "react";
+import girlImage from "../img/girl.png";
+
 const Heading = () => {
-  return <div className="custom-header"></div>;
+  return (
+    <div className="custom-header d-flex align-items-center justify-content-around">
+      <div>
+        <div>
+          <h2 className="h2-custom">Pin The Mood</h2>
+          <h3 className="pt-3 h3-custom">
+            Your Personal Recommendation Diary
+            <br /> with a Mood Twist
+          </h3>
+        </div>
+      </div>
+      <div className="align-self-end">
+        <img src={girlImage} alt="girl" className="galinha" />
+      </div>
+    </div>
+  );
 };
+
 export default Heading;
--- client/src/Components/Table.js
+++ client/src/Components/Table.js
@@ -1,3 +1,51 @@
-const Table = () => {};
+import Form from "react-bootstrap/Form";
+
+const Table = () => {
+  return (
+    <section className="table">
+      <div className="table__header">
+        <Form.Group>
+          <Form.Control type="text" placeholder="Search..." />
+        </Form.Group>
+      </div>
+      <div className="table__body">
+        <table>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Title</th>
+              <th>Medium</th>
+              <th>Mood</th>
+              <th>Recommended by</th>
+              <th>Rating</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>template</td>
+              <td>template</td>
+              <td>template</td>
+              <td>template</td>
+              <td>
+                <strong>template</strong>
+              </td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>template</td>
+              <td>template</td>
+              <td>template</td>
+              <td>template</td>
+              <td>
+                <strong>template</strong>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </section>
+  );
+};
 
 export default Table;
